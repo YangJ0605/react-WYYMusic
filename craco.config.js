f63ecf14f559c41810f46a69074c4608280cf202
YangJ0605 <1442122744@qq.com>
2020-09-05 23:13:22 +0800

    ts下配置置别名

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,18 @@
 const path = require('path')
+const CracoAlias = require('craco-alias')
 
 const resolve = dir => path.resolve(__dirname, dir)
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@': resolve('src'),
-      'components': resolve('src/components'),
-      
+  plugins: [
+    // 别名设置
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: "./tsconfig.extend.json"
+      }
     }
-  }
+  ]
 }
