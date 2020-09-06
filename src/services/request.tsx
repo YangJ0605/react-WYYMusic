5621f5c9998c84c5004388dcdbba39c6fe4b55e9
YangJ0605 <1442122744@qq.com>
2020-09-06 22:19:51 +0800

    封装axios

---
+++ src/services/request.tsx
@@ -0,0 +1,53 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import { message } from 'antd'
+
+import { TIMEOUT, baseUrl } from './config'
+
+
+const instance: AxiosInstance = axios.create({
+  timeout: TIMEOUT,
+  baseURL: baseUrl
+})
+
+instance.interceptors.request.use(config => {
+  return config
+}, err => console.log(err))
+
+instance.interceptors.response.use(response => {
+  return response.data
+}, err => {
+  if (err && err.response) {
+    switch (err.response.status) {
+      case 400:
+        console.log("请求错误");
+        break;
+      case 401:
+        console.log("未授权访问");
+        break;
+      default:
+        console.log("其他错误信息");
+    }
+  }
+  return err
+})
+
+const request = (url: string, data = {}, method = 'get') => {
+  method = method.toLocaleLowerCase()
+  return new Promise(resolve => {
+    let promise: Promise<AxiosResponse<any>>
+    if (method === 'get') {
+      promise = axios.get(url, {
+        params: data
+      })
+    } else {
+      promise = axios.post(url, data)
+    }
+    promise.then(res => {
+      resolve(res)
+    }).catch(err => {
+      message.error('请求出错' + err.message)
+    })
+  })
+}
+
+export default request
