c416e085a340646a1f580c37d8b9c712a109e3ce
YangJ0605 <1442122744@qq.com>
2020-09-08 23:19:23 +0800

    finish banner

--- src/pages/discover/c-pages/recommend/index.tsx
+++ src/pages/discover/c-pages/recommend/index.tsx
@@ -1,6 +1,11 @@
 import React, { memo} from 'react'
 
-import {RecommendWrapper} from './style'
+import {
+  RecommendWrapper,
+  Content,
+  RecommendLeft,
+  RecommendRight
+} from './style'
 import TopBanner from './c-cpns/top-banner'
 
 export default memo(function Recommend() {
@@ -8,6 +13,10 @@ export default memo(function Recommend() {
   return (
     <RecommendWrapper>
       <TopBanner/>
+      <Content>
+        <RecommendLeft></RecommendLeft>
+        <RecommendRight></RecommendRight>
+      </Content>
     </RecommendWrapper>
   )
 })
